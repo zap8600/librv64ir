c6142f27177dfdd3efeb1ed7a261e6f891943ff6
zap8600 <95487746+zap8600@users.noreply.github.com>
2023-10-03 13:58:31 -0500

    hol on

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,23 @@
+mod bus;
+mod clint;
+mod cpu;
+mod dram;
+mod plic;
+mod trap;
+mod uart;
+mod virtio;
+
+use crate::cpu::*;
+use crate::trap::*;
+
+#[repr(C)]
+pub struct CpuState {
+    pub regs: [u64; 32],
+    pub pc: u64,
+    pub csrs: [u64; 4096],
+}
+
 #[no_mangle]
-pub extern fn print_hello_from_rust() {
+pub extern fn rv64i_cycle() -> CpuState {
     println!("Hello from Rust");
 }
